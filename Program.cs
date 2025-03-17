afb9a568578b79e1e9de8c44335b816aa5f8f4c7
NomenConservandum <hramov.sanya2018@yandex.ru>
2025-03-17 11:12:23 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// WOLSAR English club Telegram Bot source code
+
+using Telegram.Bot;
+using Sensitive;
+Variables sensitive = new Variables();
+
+var bot = new TelegramBotClient(sensitive.getToken());
+var me = await bot.GetMe();
+Console.WriteLine($"The ID of the bot is {me.Id}\nThe name of the bot is {me.FirstName}.");
