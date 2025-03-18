491becef6ce70146240b7a6fdf17d8ac18b4e7d6
NomenConservandum <hramov.sanya2018@yandex.ru>
2025-03-18 09:10:10 +0400

    Implement sendInlineURL command

--- BasicCommands.cs
+++ BasicCommands.cs
@@ -1,4 +1,8 @@
 using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Exceptions;
 
 namespace BasicCommands {
     public class Commands {
@@ -8,11 +12,39 @@ namespace BasicCommands {
         }
 
         public void sendMsg(long chatID, String msg) {
-            botClient.SendTextMessageAsync(
+            botClient.SendMessage(
                 chatID,
                 msg
             );
             return;
+        }
+        public void sendMsgInline(long chatID, String msg, InlineKeyboardMarkup ikm) {
+            botClient.SendMessage(
+                    chatID,
+                    msg,
+                    replyMarkup: ikm
+                    );
+            return;
+        }
+        // Either user clicks on the link or sends 'NEGATIVE' via callBackQuery
+        public void sendInlineURL(long chatID, String msg, String msgLink, String msgNegative, String url) {
+                var inlineKeyboard = new InlineKeyboardMarkup(
+                    new List<InlineKeyboardButton[]>() {
+                        new InlineKeyboardButton[] {
+                            InlineKeyboardButton.WithUrl(msgLink, url),
+                        },
+                        new InlineKeyboardButton[] {
+                            InlineKeyboardButton.WithCallbackData(msgNegative, "NEGATIVE"), 
+                        },
+                    }
+                );
+                sendMsgInline(
+                    chatID,
+                    msg,
+                    inlineKeyboard
+                );
+                
+                return;
         }
     }
 }
--- BotAPI.cs
+++ BotAPI.cs
@@ -1,6 +1,7 @@
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Exceptions;
 using Sensitive;
@@ -10,13 +11,21 @@ namespace BotAPI {
     public class Bot {
     private static ITelegramBotClient botClient;
     private static ReceiverOptions receiverOptions = new ReceiverOptions();
+    private static String inviteURL = "";
+    private static Commands commands;
     public static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken) {
-        Commands commands = new Commands(botClient);
         try {
             switch (update.Type) {
                 case UpdateType.Message: {
                     Console.WriteLine($"There is a new message from {update?.Message?.From?.Username}!\nIt goes, \'{update?.Message?.Text}\'");
-                    commands.sendMsg(update.Message.Chat.Id, $"You\'ve said: \n\'{update?.Message?.Text}\'");
+                    commands.sendInlineURL(
+                        update.Message.Chat.Id,
+                        "I\'d like to invite you to our chat!\nThe link is here:", 
+                        "Your link :D",
+                        "No, thank you",
+                        inviteURL
+                    );
+                    
                     return;
                 }
             }
@@ -26,6 +35,7 @@ namespace BotAPI {
         }
     }
 
+    // Not my code, lol
     public static Task ErrorHandler(ITelegramBotClient botClient, Exception error, CancellationToken cancellationToken) {
         // Тут создадим переменную, в которую поместим код ошибки и её сообщение 
         var ErrorMessage = error switch {
@@ -37,7 +47,8 @@ namespace BotAPI {
         Console.WriteLine(ErrorMessage);
         return Task.CompletedTask;
     }
-    
+    // Not my code block ends
+
     // Should be used with 'await' keyword
     public System.Threading.Tasks.Task<Telegram.Bot.Types.User> getMe() {
         return botClient.GetMe();
@@ -45,13 +56,16 @@ namespace BotAPI {
     
     public Bot() {
         Variables sensitive = new Variables();
+        inviteURL = sensitive.getURL();
         botClient = new TelegramBotClient(sensitive.getToken());
+        commands = new Commands(botClient);
+        
         receiverOptions.AllowedUpdates = new[] {
             UpdateType.Message,
             UpdateType.CallbackQuery,
         };
         receiverOptions.DropPendingUpdates = true; // we ignore all of the messages that were sent when the bot was offline
-         
+        
         var cts = new CancellationTokenSource();
         botClient.StartReceiving(UpdateHandler, ErrorHandler, receiverOptions, cts.Token); // The bot is online
         }
